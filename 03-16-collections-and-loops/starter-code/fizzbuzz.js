28ed837c708a2e3661d88d6dc5fc41ef4599e5f9
Johnny Ray Austin <johnny@johnnyray.me>
2016-03-13 17:52:50 -0400

    initial content

---
+++ 03-16-collections-and-loops/starter-code/fizzbuzz.js
@@ -0,0 +1,35 @@
+/**
+ * FizzBuzz Excercise
+
+ Relying on newfound knowledge of loops, combine loops and if/else statements together and incrementally build the common fizzbuzz loop.
+
+ - In the loop every time a number is divisible by **3**, instead of logging the number itself, the word "fizz" should appear.
+ - If the number is divisible by  **5**, the word "buzz" should be logged.
+ - If the number is divisible by both **3** and  **5**, then the word "fizzbuzz" should be logged.
+
+ Follow the steps below.
+
+ ##### Step 1:
+ Construct a for loop that iterates through, and `console.log()`'s out, numbers 1 - 100.
+
+
+
+ ##### Step 2:
+ Add an `if/else` statement that logs the string `"fizz"` if the value being iterated over is divisible by `3`; otherwise, log out the value.
+
+
+
+ ##### Step 3:
+ Add an `else if` clause that logs the string `"buzz"` if the value being iterated over is divisible by `5`.
+
+
+
+ ##### Step 4:
+ Add an additional `else if` clause that logs the string `"fizzbuzz"` if the value being iterated over is divisible by both `3` and `5`. __Note:__ this step is intentionally broken! Place the new `else if` __below__ the evaluations for `fizz` and `buzz`; after running the code, and experiencing the undesired results, prompt the students as to why the `fizzbuzz` evaluation never occurred.
+
+
+ ##### Step 5:
+ Fix the above code to evaluate the `fizzbuzz` condition:
+
+
+ */
