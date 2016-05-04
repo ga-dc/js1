dc3535b5665b4dae76f4e212437a020c03e97f61
Johnny Austin <johnny@johnnyray.me>
2016-05-03 21:28:28 -0400

    starter code

--- 05-04-CRUD/solution-code/app.js
+++ 05-04-CRUD/solution-code/app.js
@@ -1,115 +1,101 @@
+
+var Message = TypeFactory('Message');
+
 $(document).ready(function() {
-
-  // connect to your Firebase application using your reference URL
-  var messageAppReference = new Firebase("https://js-dev-test.firebaseio.com/");
-
   $('#message-form').submit(function(event) {
     // by default a form submit reloads the DOM which will subsequently reload all our JS
     // to avoid this we preventDefault()
-    event.preventDefault()
-
+    event.preventDefault();
     // grab user message input
-    var message = $('#message').val()
+    var message = $('#message').val();
 
     // clear message input (for UX purposes)
-    $('#message').val('')
+    $('#message').val('');
 
     // create a section for messages data in your db
-    var messagesReference = messageAppReference.child('messages');
+    var messageObj = {
+      text: message,
+      upVotes: 0
+    };
 
-    // use the set method to save data to the messages
-    messagesReference.push({
-      message: message,
-      votes: 0
-    })
+    Message.create({ text: message, upVotes: 0, downVotes: 0 }, function(err, result) {
+      if (err) {
+        console.error(err);
+      } else {
+        messageObj.objectId = result.objectId;
+        renderMessage(messageObj);
+      }
+    });
+  });
 
-  })
-
-  // // on initialization of app (when document is ready) get fan messages
-  getFanMessages()
+  // on initialization of app (when document is ready) get fan messages
+  getFanMessages();
 
   function getFanMessages() {
-    // retrieve messages data when .on() initially executes
-    // and when its data updates
-    messageAppReference.child('messages').on('value', function(results) {
-      var $messageBoard = $('.message-board')
-      var messages = []
 
-      // iterate through results coming from database call; messages
-      for (var msg in results.val()) {
-        // get method is supposed to represent HTTP GET method
-        var message = results.val()[msg].message
-        var votes = results.val()[msg].votes
-
-        // create message element
-        var $messageListElement = $('<li></li>')
-        // add message to li
-        $messageListElement.html(message)
-
-        // create delete element
-        var $deleteElement = $('<i class="fa fa-trash pull-right delete"></i>')
-        $deleteElement.click(function(e) {
-          var id = $(e.target.parentNode).data('id')
-          deleteMessage(id)
-        })
-
-        // add delete element
-        $messageListElement.append($deleteElement)
-
-        // create up vote element
-        var $upVoteElement = $('<i class="fa fa-thumbs-up pull-right"></i>')
-        $upVoteElement.click(function(e) {
-
-          var id = $(e.target.parentNode).data('id')
-          updateMessage(id, votes++)
-        })
-
-        // create down vote element
-        var $downVoteElement = $('<i class="fa fa-thumbs-down pull-right"></i>')
-        $downVoteElement.click(function(e) {
-          var id = $(e.target.parentNode).data('id')
-          updateMessage(id, votes--)
-        })
-
-        // add voting elements
-        $messageListElement.append($upVoteElement)
-        $messageListElement.append($downVoteElement)
-
-        // show votes
-        $messageListElement.append('<div class="pull-right">' + votes + '</div>')
-
-        // add id as data attribute so we can refer to later for updating
-        $messageListElement.attr('data-id', msg)
-
-        // push element to array of messages
-        messages.push($messageListElement)
-
-        // remove lis to avoid dupes
-        $messageBoard.empty()
-
-        for (var i in messages) {
-          $messageBoard.append(messages[i])
-        }
-
+    $('.message-board').on('click', '.fa', function(e) {
+      var $this = $(this);
+      if ($this.hasClass('vote-up')) {
+        updateVote($this.closest('.message'));
+      } else if ($this.hasClass('delete')) {
+        removePost($this.closest('.message'));
+      } else {
+        console.error('IDK');
       }
-    })
+    });
 
+    function removePost($messageEl) {
+      var messageId = $messageEl.data('id');
+
+      Message.remove(messageId, function(err) {
+        if (err) {
+          console.error(err);
+        } else {
+          $('[data-id="' + messageId + '"]').remove();
+        }
+      });
+    }
+
+    function updateVote($messageEl) {
+      var messageId = $messageEl.data('id');
+      var upVotes = $messageEl.data('upvotes');
+
+      upVotes += 1;
+
+      Message.update(messageId, { upVotes: upVotes }, function(err, result) {
+        if (err) {
+          console.error(err);
+        } else {
+          var messageObj = {
+            objectId: messageId,
+            text: $messageEl.find('.message__text').html(),
+            upVotes: upVotes
+          };
+          var html = compile(messageObj);
+          $('[data-id="' + messageId + '"]').html(html);
+        }
+      });
+    }
+
+    Message.getAll(function(err, messages) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(messages)
+        messages.forEach(renderMessage);
+      }
+    });
   }
 
-  function updateMessage(id, votes) {
-    // find message whose objectId is equal to the id we're searching with
-    var messageReference = new Firebase('https://js-dev-test.firebaseio.com/messages/' + id)
-
-    // update votes property
-    messageReference.update({
-      votes: votes
-    })
+  function renderMessage(messageData) {
+    var html = compile(messageData);
+    $('.message-board').append(html);
   }
 
-  function deleteMessage(id) {
-    // find message whose objectId is equal to the id we're searching with
-    var messageReference = new Firebase('https://js-dev-test.firebaseio.com/messages/' + id)
-
-    messageReference.remove()
+  function compile(messageData) {
+    var source = $("#message-template").html();
+    var template = Handlebars.compile(source);
+    var html = template(messageData);
+    return html;
   }
-})
+});
